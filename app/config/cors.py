e46745a3371b01fb3f858d0ba8272c1723ef077e
MuhamadAdrian <madrian.septiana@gmail.com>
2025-05-29 00:20:46 +0700

    chore: cors config

---
+++ app/config/cors.py
@@ -0,0 +1,18 @@
+# app/config/cors.py
+
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+
+def add_cors(app: FastAPI):
+    origins = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.api.v1 import routes
+from app.config.cors import add_cors
 
 app = FastAPI()
 app.include_router(routes.router)
+add_cors(app)
